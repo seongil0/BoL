b84761889905a9a4e0223abd22ad58b667e2cd1c
UglyOldGuy <mariozeph@gmail.com>
2013-11-09 12:14:38 -0500

    1.3 New updates read changelog

--- SidasAutoCarryPlugin - Katarina.lua
+++ SidasAutoCarryPlugin - Katarina.lua
@@ -1,5 +1,6 @@
 ﻿--[[
-	AutoCarry Script - Katarina 1.2 by Skeem
+	AutoCarry Script - Katarina 1.3 by Skeem
+		With Code from Kain <3
 
 	Changelog :
    1.0 - Initial Release
@@ -10,7 +11,12 @@
 	   - More Damage Calculation Adjustments
 	   - More checks for when to ult
 	   - More checks to not use W when enemy not in range
- 	]] --		
+   1.2.1 - Fixed the problem with channelling ultimate
+   1.3 - Fixed the problem with ult AGAIN
+       - Added Auto Pots
+	   - Added Auto Zhonyas
+	   - Added Draw Circles of targets that can die
+  	]] --		
 
 -- Hero Name Check
 if myHero.charName ~= "Katarina" then return end
@@ -28,43 +34,59 @@ end
 
 --[Plugin OnTick]--
 function PluginOnTick()
-	if isChanneling("Spell4") then
-        AutoCarry.CanAttack = false
-        AutoCarry.CanMove = false
-			else
-        AutoCarry.CanAttack = true
-        AutoCarry.CanMove = true
+	if isChanneling() then
+		if IsSACReborn then
+			AutoCarry.MyHero:MovementEnabled(false)
+			AutoCarry.MyHero:AttacksEnabled(false)
+        else
+			AutoCarry.CanAttack = false
+			AutoCarry.CanMove = false
+		end
+	else
+		if IsSACReborn then
+			AutoCarry.MyHero:MovementEnabled(true)
+			AutoCarry.MyHero:AttacksEnabled(true)
+        else
+			AutoCarry.CanAttack = true
+			AutoCarry.CanMove = true
+		end
     end
 	Checks()
 	smartKS()
+	if Menu.AutoLevelSkills then autoLevelSetSequence(levelSequence) end
 	if Menu.hHK then Harrass() end
 	if Menu.bCombo and Carry.AutoCarry then bCombo() end
 	if not Menu.mFarm and not Carry.AutoCarry then Farm() end
-	if Menu.wHarrass and Target and GetDistance(Target) <= wRange then
-		CastSpell(_W)
-	end
+	if Menu.wHarrass and Target and GetDistance(Target) <= wRange then CastSpell(_W) end
+	
+	if Extras.ZWItems and IsMyHealthLow() and Target and (ZNAREADY or WGTREADY) then CastSpell((wgtSlot or znaSlot)) end
+	if Extras.aHP and NeedHP() and not (UsingHPot or UsingFlask) and (HPREADY or FSKREADY) then CastSpell((hpSlot or fskSlot)) end
 end
 --[/Plugin OnTick]--
 
 --[Farm Function]--
 function Farm()
 	for _, minion in pairs(AutoCarry.EnemyMinions().objects) do
+		local qDmg = getDmg("Q",minion,myHero)
+        local wDmg = getDmg("W",minion,myHero)
+		local eDmg = getDmg("E",minion,myHero)
 		if ValidTarget(minion) then
 			if Menu.qFarm and QREADY and GetDistance(minion) <= qRange then
-				if minion.health <= getDmg("Q", minion, myHero) then CastSpell(_Q, minion) end
+				if qDmg >= minion.health then CastSpell(_Q, minion) end
 			end
 			if Menu.wFarm and WREADY and GetDistance(minion) <= wRange then
-				if minion.health < getDmg("W", minion, myHero) then CastSpell(_W, minion) end
+				if wDmg >= minion.health then CastSpell(_W) end
 			end
 			if Menu.eFarm and EREADY and GetDistance(minion) <= eRange then
-				if minion.health <= getDmg("E", minion, myHero) then CastSpell(_E, minion) end
+				if eDmg >= minion.health then CastSpell(_E, minion) end
 			end
 		end
+		
 	end
 end
 --[/Farm Function]--
 
---[Harrass Function]--
+--[Harass Function]--
 function Harrass()
 	if Target and Menu.hHK then
 		if Menu.hMode == 1 then
@@ -78,7 +100,7 @@ function Harrass()
 		end
 	end
 end
---[/Harrass Function]--
+--[/Harass Function]--
 
 --[Burst Combo Function]--
 function bCombo()
@@ -104,7 +126,7 @@ function smartKS()
 			qDmg = getDmg("Q",enemy,myHero)
             wDmg = getDmg("W",enemy,myHero)
 			eDmg = getDmg("E",enemy,myHero)
-            rDmg = getDmg("R",enemy,myHero)*10
+            rDmg = getDmg("R",enemy,myHero)*9
 			if DFGREADY then dfgDmg = (dfgSlot and getDmg("DFG",enemy,myHero) or 0)	end
             if HXGREADY then hxgDmg = (hxgSlot and getDmg("HXG",enemy,myHero) or 0) end
             if BWCREADY then bwcDmg = (bwcSlot and getDmg("BWC",enemy,myHero) or 0) end
@@ -116,6 +138,32 @@ function smartKS()
 			--if Menu.debug then PrintChat("rDmg"..rDmg) end	
 			------- DEBUG --------
 			if Menu.sKS then
+				if enemy.health <= (qDmg) and GetDistance(enemy) <= qRange and QREADY then
+					if QREADY then CastSpell(_Q, enemy) end
+				end
+				if enemy.health <= (wDmg) and GetDistance(enemy) <= wRange and WREADY then
+					if WREADY then CastSpell(_W, enemy) end
+				end
+				if enemy.health <= (eDmg) and GetDistance(enemy) <= eRange and EREADY then
+					if EREADY then CastSpell(_E, enemy) end
+				end
+				if enemy.health <= (qDmg + wDmg) and GetDistance(enemy) <= wRange and WREADY and QREADY then
+					if QREADY then CastSpell(_Q, enemy) end
+					if WREADY then CastSpell(_W, enemy) end
+				end
+				if enemy.health <= (qDmg + eDmg) and GetDistance(enemy) <= eRange and QREADY and EREADY then
+					if QREADY then CastSpell(_Q, enemy) end
+					if EREADY then CastSpell(_E, enemy) end
+				end
+				if enemy.health <= (wDmg + eDmg) and GetDistance(enemy) <= wRange and WREADY and EREADY then
+					if EREADY then CastSpell(_E, enemy) end
+					if WREADY then CastSpell(_W, enemy) end
+				end
+				if enemy.health <= (qDmg + eDmg + wDmg) and GetDistance(enemy) <= eRange and EREADY and QREADY and WREADY then
+					if QREADY then CastSpell(_Q, enemy) end
+					if EREADY then CastSpell(_E, enemy) end
+					if WREADY then CastSpell(_W, enemy) end
+				end
 				if enemy.health <= (qDmg + itemsDmg) and GetDistance(enemy) <= qRange and QREADY then
 					if DFGREADY then CastSpell(dfgSlot, enemy) end
 					if HXGREADY then CastSpell(hxgSlot, enemy) end
@@ -198,6 +246,13 @@ function smartKS()
 					if IREADY then CastSpell(ignite, enemy) end
 				end
 			end
+			KillText[i] = 1 
+			if enemy.health <= (qDmg + eDmg + wDmg + itemsDmg) and QREADY and WREADY and EREADY then
+			KillText[i] = 2
+			end
+			if enemy.health <= (qDmg + eDmg + wDmg + rDmg + itemsDmg) and QREADY and WREADY and EREADY and RREADY then
+			KillText[i] = 3
+			end
 		end
 	end
 end
@@ -210,28 +265,95 @@ function PluginOnAnimation(unit, animationName)
 end
 --[/Plugin OnAnimation - Credits: Λnonymous]--
 
---[Channeling Function - Credits: Λnonymous]--
-function isChanneling(animationName)
-        if lastAnimation == animationName then
+--[Channelling Function - Credits: Λnonymous]--
+function isChanneling()
+        if lastAnimation == "Spell4" then
                 return true
         else
                 return false
         end
 end
---[/Channeling Function]--
+--[/Channelling Function]--
+
+function IsMyHealthLow()
+	if myHero.health < (myHero.maxHealth * ( Extras.ZWHealth / 100)) then
+		return true
+	else
+		return false
+	end
+end
+--[Health Pots Function]--
+function NeedHP()
+	if myHero.health < (myHero.maxHealth * ( Extras.HPHealth / 100)) then
+		return true
+	else
+		return false
+	end
+end
+
+function PluginOnCreateObj(obj)
+	if obj ~= nil then
+		if obj.name:find("TeleportHome.troy") then
+			if GetDistance(obj, myHero) <= 70 then
+				Recall = true
+			end
+		end
+		if obj.name:find("Global_Item_HealthPotion.troy") then
+			if GetDistance(obj, myHero) <= 70 then
+				UsingHPot = true
+				UsingFlask = true
+			end
+		end
+		if obj.name:find("Global_Item_ManaPotion.troy") then
+			if GetDistance(obj, myHero) <= 70 then
+				UsingFlask = true
+				UsingMPot = true
+			end
+		end
+	end
+end
+
+function PluginOnDeleteObj(obj)
+	if obj.name:find("TeleportHome.troy") then
+		Recall = false
+	end
+	if obj.name:find("Global_Item_HealthPotion.troy") then
+		UsingHPot = false
+		UsingFlask = false
+	end
+	if obj.name:find("Global_Item_ManaPotion.troy") then
+		UsingMPot = false
+		UsingFlask = false
+	end
+end
 
 --[Plugin OnDraw]--
 function PluginOnDraw()
 	--> Ranges
 	if not Menu.mDraw and not myHero.dead then
 		if QREADY and Menu.qDraw then 
-			DrawCircle(myHero.x, myHero.y, myHero.z, qRange, 0x191970)
+			DrawCircle(myHero.x, myHero.y, myHero.z, qRange, 0xB20000)
 		end
 		if WREADY and Menu.wDraw then
 			DrawCircle(myHero.x, myHero.y, myHero.z, wRange, 0x20B2AA)
 		end
 		if EREADY and Menu.eDraw then
 			DrawCircle(myHero.x, myHero.y, myHero.z, eRange, 0x800080)
+		end
+	end
+	if Menu.cDraw then
+		for i=1, heroManager.iCount do
+			local Unit = heroManager:GetHero(i)
+			if ValidTarget(Unit) then
+				if waittxt[i] == 1 and (KillText[i] ~= nil or 0 or 1) then
+					PrintFloatText(Unit, 0, TextList[KillText[i]])
+				end
+			end
+			if waittxt[i] == 1 then
+				waittxt[i] = 30
+			else
+				waittxt[i] = waittxt[i]-1
+			end
 		end
 	end
 end
@@ -241,9 +363,16 @@ end
 function mainLoad()
 	qRange, wRange, eRange, rRange = 675, 375, 700, 550
 	QREADY, WREADY, EREADY, RREADY = false, false, false, false
-	lastAnimation = nil
+	lastAnimation = "Run"
 	Menu = AutoCarry.PluginMenu
 	Carry = AutoCarry.MainMenu
+	levelSequence = { 1,3,2,2,2,4,2,1,2,1,4,1,1,3,3,4,3,3 }
+	TextList = {"Harass him!!", "Q+E KILL!!", "FULL COMBO KILL!"}
+	KillText = {}
+	waittxt = {} -- prevents UI lags, all credits to Dekaron
+	UsingHPot = false
+	for i=1, heroManager.iCount do waittxt[i] = i*3 end -- All credits to Dekaron
+	if AutoCarry.Skills then IsSACReborn = true else IsSACReborn = false end
 end
 --[/Function mainLoad]--
 
@@ -263,9 +392,17 @@ function mainMenu()
 	Menu:addParam("eFarm", "Farm with Shunpo (E)", SCRIPT_PARAM_ONOFF, false)
 	Menu:addParam("sep3", "-- Draw Options --", SCRIPT_PARAM_INFO, "")
 	Menu:addParam("mDraw", "Disable All Ranges Drawing", SCRIPT_PARAM_ONOFF, false)
+	Menu:addParam("cDraw", "Draw Enemy Text", SCRIPT_PARAM_ONOFF, true)
 	Menu:addParam("qDraw", "Draw Bouncing Blades (Q) Range", SCRIPT_PARAM_ONOFF, true)
 	Menu:addParam("wDraw", "Draw Sinister Steel (W) Range", SCRIPT_PARAM_ONOFF, true)
 	Menu:addParam("eDraw", "Draw Shunpo (E) Range", SCRIPT_PARAM_ONOFF, true)
+	Menu:addParam("AutoLevelSkills", "Auto Level Skills (Requires Reload)", SCRIPT_PARAM_ONOFF, true)
+	Extras = scriptConfig("Sida's Auto Carry Plugin: "..myHero.charName..": Extras", myHero.charName)
+	Extras:addParam("sep6", "-- Misc --", SCRIPT_PARAM_INFO, "")
+	Extras:addParam("ZWItems", "Auto Zhonyas/Wooglets", SCRIPT_PARAM_ONOFF, true)
+	Extras:addParam("ZWHealth", "Min Health % for Zhonyas/Wooglets", SCRIPT_PARAM_SLICE, 15, 0, 100, -1)
+	Extras:addParam("aHP", "Auto Health Pots", SCRIPT_PARAM_ONOFF, true)
+	Extras:addParam("HPHealth", "Min % for Health Pots", SCRIPT_PARAM_SLICE, 50, 0, 100, -1)
 	----------- DEBUG ----------
 	--Menu:addParam("debug", "Debugging Prints", SCRIPT_PARAM_ONKEYDOWN, false, 88)
 end
@@ -275,9 +412,11 @@ end
 function Checks()
 	if myHero:GetSpellData(SUMMONER_1).name:find("SummonerDot") then ignite = SUMMONER_1
 	elseif myHero:GetSpellData(SUMMONER_2).name:find("SummonerDot") then ignite = SUMMONER_2 end
-	Target = AutoCarry.GetAttackTarget(true)
+	if IsSACReborn then Target = AutoCarry.Crosshair:GetTarget(true) else Target = AutoCarry.GetAttackTarget(true) end
 	dfgSlot, hxgSlot, bwcSlot = GetInventorySlotItem(3128), GetInventorySlotItem(3146), GetInventorySlotItem(3144)
 	brkSlot = GetInventorySlotItem(3092),GetInventorySlotItem(3143),GetInventorySlotItem(3153)
+	znaSlot, wgtSlot = GetInventorySlotItem(3157),GetInventorySlotItem(3090)
+	hpSlot, mpSlot, fskSlot = GetInventorySlotItem(2003),GetInventorySlotItem(2004),GetInventorySlotItem(2041)
 	QREADY = (myHero:CanUseSpell(_Q) == READY)
 	WREADY = (myHero:CanUseSpell(_W) == READY)
 	EREADY = (myHero:CanUseSpell(_E) == READY)
@@ -286,6 +425,11 @@ function Checks()
 	HXGREADY = (hxgSlot ~= nil and myHero:CanUseSpell(hxgSlot) == READY)
 	BWCREADY = (bwcSlot ~= nil and myHero:CanUseSpell(bwcSlot) == READY)
 	BRKREADY = (brkSlot ~= nil and myHero:CanUseSpell(brkSlot) == READY)
+	ZNAREADY = (znaSlot ~= nil and myHero:CanUseSpell(znaSlot) == READY)
+	WGTREADY = (wgtSlot ~= nil and myHero:CanUseSpell(wgtSlot) == READY)
 	IREADY = (ignite ~= nil and myHero:CanUseSpell(ignite) == READY)
+	HPREADY = (hpSlot ~= nil and myHero:CanUseSpell(hpSlot) == READY)
+	MPREADY =(mpSlot ~= nil and myHero:CanUseSpell(mpSlot) == READY)
+	FSKREADY = (fskSlot ~= nil and myHero:CanUseSpell(fskSlot) == READY)
 end
 --[/Cooldown Checks]--
